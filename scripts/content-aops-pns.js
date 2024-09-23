2d7cf6509ddcee48aed362c8455b6b8a7adbf525
rythm <jgon@mit.edu>
2024-09-23 10:51:40 -0500

    parse problem_and_solution pages

--- scripts/content-aops-pns.js
+++ scripts/content-aops-pns.js
@@ -1,1 +1,12 @@
-console.log('hi')
+mwBody = document.getElementsByClassName('mw-body')[0];
+potentTestNodes = mwBody.querySelectorAll('td, li');
+
+testNodes = Array.from(potentTestNodes)
+  .filter(testNode => testNode.getElementsByTagName('a').length);
+
+testNodes.forEach(testNode => {
+  statusText = document.createElement('p')
+  statusText.textContent = 'Unstarted'
+
+  testNode.appendChild(statusText);
+});
