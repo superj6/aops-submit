63813779ba386296a6be755a6128c1bf176d58c8
rythm <jgon@mit.edu>
2024-09-23 15:59:32 -0500

    content parse testpage, fix content-script scope, testlist parse fixes

--- scripts/content-testlist.js
+++ scripts/content-testlist.js
@@ -1,12 +1,13 @@
 mwBody = document.getElementsByClassName('mw-body')[0];
-potentTestNodes = mwBody.querySelectorAll('td, li');
+testList = mwBody.getElementsByTagName('tbody')[0] || mwBody.getElementsByTagName('ul')[0]
+potentTestNodes = testList.querySelectorAll('td, li');
 
 testNodes = Array.from(potentTestNodes)
-  .filter(testNode => testNode.getElementsByTagName('a').length);
+    .filter(testNode => testNode.getElementsByTagName('a').length);
 
 testNodes.forEach(testNode => {
   statusText = document.createElement('p');
-  statusText.textContent = 'Unstarted';
+  statusText.textContent = 'Not started';
 
   testNode.appendChild(statusText);
 });
--- scripts/content-testpage.js
+++ scripts/content-testpage.js
@@ -1,1 +1,21 @@
-console.log('hi')
+mwBody = document.getElementsByClassName('mw-body')[0];
+problemList = mwBody.querySelectorAll('ul ul')[0] 
+    || mwBody.getElementsByTagName('ul')[0];
+problemNodes = Array.from(problemList.getElementsByTagName('li'));
+testNode = problemList.parentElement.parentElement.querySelectorAll(':scope > li')[0] 
+    || mwBody.getElementsByTagName('p')[1];
+
+
+(() => {
+  statusText = document.createElement('p');
+  statusText.textContent = 'Not started';
+
+  testNode.appendChild(statusText);
+})();
+
+problemNodes.forEach(problemNode => {
+  statusText = document.createElement('p');
+  statusText.textContent = 'No submission';
+
+  problemNode.appendChild(statusText);
+});
