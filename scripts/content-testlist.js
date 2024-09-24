7031b1c4fbe053d740f6ad0275a70d14ac70cdef
rythm <jgon@mit.edu>
2024-09-24 15:39:23 -0500

    display status on testlist

--- scripts/content-testlist.js
+++ scripts/content-testlist.js
@@ -1,13 +1,40 @@
-mwBody = document.getElementsByClassName('mw-body')[0];
-testList = mwBody.getElementsByTagName('tbody')[0] || mwBody.getElementsByTagName('ul')[0]
-potentTestNodes = testList.querySelectorAll('td, li');
+const mwBody = document.getElementsByClassName('mw-body')[0];
+const testList = mwBody.getElementsByTagName('tbody')[0] || mwBody.getElementsByTagName('ul')[0]
+const potentTestNodes = testList.querySelectorAll('td, li');
 
-testNodes = Array.from(potentTestNodes)
-  .filter(testNode => testNode.getElementsByTagName('a').length);
+var utils;
 
-testNodes.forEach(testNode => {
+async function initImports(){
+  const src = chrome.runtime.getURL("/scripts/utils.js");
+  utils = await import(src);
+} 
+
+function getTestNodes(){
+  return Array.from(potentTestNodes)
+    .filter(testNode => testNode.getElementsByTagName('a').length);
+}
+
+async function displayTestStatus(testNode){
+  const testUrl = `${testNode.getElementsByTagName('a')[0].href}_Problems`;
+  const testStatus = await utils.getTestStatus(testUrl);
+
   statusText = document.createElement('p');
-  statusText.textContent = 'Not started';
+  if(testStatus){
+    let total = testStatus.length;
+    let correct = testStatus.filter(status => status === 2).length;
+    statusText.textContent = `${correct}/${total} complete`;
+  }else{
+    statusText.textContent = 'Not started';
+  }
 
   testNode.appendChild(statusText);
-});
+}
+
+async function main(){
+  await initImports();
+
+  const testNodes = getTestNodes();
+  testNodes.forEach(displayTestStatus);
+}
+
+main();
